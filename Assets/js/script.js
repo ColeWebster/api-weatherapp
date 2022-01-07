b872fef0cc23eb5fbf21e5901359d5c596dc3ce1
Cole Webster <Cole.Webster@gmail.com>
2022-01-07 14:59:55 -0500

    Updating JS file with code from end of class

---
+++ Assets/js/script.js
@@ -0,0 +1,27 @@
+//function that will save to local storage
+//a way to pull items from local storage
+//Utilize the weather api
+//Use information from API to build the display (BOOTSTRAP)
+// Needs to dynamically change based on the information pull by the API (aka each city)
+
+
+// -----Global variables
+var searchHistory = [];
+var weatherApiURL = 'https://api.openweathermap.org'
+var weatherApiKey = 
+
+
+// ---------DOM Elements
+// Main card
+// Search button
+
+// ------Functions
+// renderSearchHistory
+// Append to search history
+// Render the weather
+// Render the forecast card
+// Render the forecast
+// fetch from the API the weather
+// Fetch the coordinates of the city
+// Handle form submit
+// Click the cities in search history to reappear
