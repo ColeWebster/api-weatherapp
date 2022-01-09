6b3d26de519aa45dcf095ad3c96174011b3806c8
Cole's Laptop <cole.webster@gmail.com>
2022-01-09 13:16:12 -0500

    Updating work from laptop, working to get the JS file to save and post a list

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -9,100 +9,72 @@
 var weatherApiURL = "https://api.openweathermap.org";
 var weatherApiKey = "145642453194c5fbabaae41a36e299b3";
 
-var todoInput = document.querySelector("#todo-text");
-var todoForm = document.querySelector("#todo-form");
-var todoList = document.querySelector("#todo-list");
-var todoCountSpan = document.querySelector("#todo-count");
+var searchInput = document.querySelector("#search-text");
+var searchForm = document.querySelector("#search-form");
+var searchList = document.querySelector("#search-list");
+var searchCount = document.querySelector("#search-count");
 
-var todos = [];
+var cities = [];
+//todos
 
-// The following function renders items in a todo list as <li> elements
-function renderTodos() {
-  // Clear todoList element and update todoCountSpan
-  todoList.innerHTML = "";
-  todoCountSpan.textContent = todos.length;
+//renderTodos
+function renderSearch() {
+  searchList.innerHTML = "";
+  searchCountSpan.textcontent = cities.length;
 
-  // Render a new li for each todo
-  for (var i = 0; i < todos.length; i++) {
-    var todo = todos[i];
+  for (var i = 0; i < cities.length; i++) {
+    var city = cities[i];
 
-    var li = document.createElement("li");
-    li.textContent = todo;
-    li.setAttribute("data-index", i);
+    var li = document.createElement('li');
+    li.textContent = city
+    li.setAttribute('data-index', i);
 
-    var button = document.createElement("button");
-    button.textContent = "Complete ✔️";
+    var button = document.createElement('button');
+    button.textContent = "Remove";
 
     li.appendChild(button);
-    todoList.appendChild(li);
+    searchList.appendChild(li);
   }
 }
 
-// This function is being called below and will run when the page loads.
 function init() {
-  // Get stored todos from localStorage
-  var storedTodos = JSON.parse(localStorage.getItem("todos"));
-
-  // If todos were retrieved from localStorage, update the todos array to it
-  if (storedTodos !== null) {
-    todos = storedTodos;
+  var storedCities = JSON.parse(localStorage.getitem('cities'));
+  if (storedCities !== null) {
+    cities = storedCities;
   }
-
-  // This is a helper function that will render todos to the DOM
-  renderTodos();
+  renderSearch();
 }
 
-function storeTodos() {
-  // Stringify and set key in localStorage to todos array
-  localStorage.setItem("todos", JSON.stringify(todos));
+function storedCities() {
+  localStorage.setItem("cities", JSON.stringify(cities));
 }
 
-// Add submit event to form
-todoForm.addEventListener("submit", function(event) {
+todoForm.addEventListener('submit', function (event) {
   event.preventDefault();
-
-  var todoText = todoInput.value.trim();
-
-  // Return from function early if submitted todoText is blank
-  if (todoText === "") {
+  var cityText = cityText.value.trim();
+  if (cityText === "") {
     return;
   }
+  cities.push(cityText);
+  searchInput.value = "";
 
-  // Add new todoText to todos array, clear the input
-  todos.push(todoText);
-  todoInput.value = "";
+  storedCities();
+  renderSearch();
 
-  // Store updated todos in localStorage, re-render the list
-  storeTodos();
-  renderTodos();
 });
 
-// Add click event to todoList element
-todoList.addEventListener("click", function(event) {
+searchList.addEventListener('click',function(event){
   var element = event.target;
 
-  // Checks if element is a button
-  if (element.matches("button") === true) {
-    // Get its data-index value and remove the todo element from the list
-    var index = element.parentElement.getAttribute("data-index");
+  if (element.matches('button') === true) {
+    var index = element.parentElement.getattribute('data-index');
     todos.splice(index, 1);
-
-    // Store updated todos in localStorage, re-render the list
-    storeTodos();
-    renderTodos();
   }
+  storedCities();
+  renderSearch();
 });
 
-// Calls init to retrieve data and render it to the page on load
 init()
-
-
-
-
-
-
-
-
 
 
 
