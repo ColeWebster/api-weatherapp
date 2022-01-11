ed057bcd197dc368d234aa69604b3b3e71b0d866
Cole Webster <Cole.Webster@gmail.com>
2022-01-11 08:52:15 -0500

    Continuing work on JS files, adding API fetch function and variables

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,19 +1,17 @@
 // -----Global variables
-var weatherApiURL = "https://api.openweathermap.org";
 var weatherApiKey = "145642453194c5fbabaae41a36e299b3";
 
-
 // Search related variables
-var city = $(".city");
-var date = $("date");
-var history = $(".history");
-var search = $(".searchBar")
+var city = $(`.city`);
+var date = $(`date`);
+var history = $(`.history`);
+var search = $(`.searchBar`);
 
 //Weather card related variables
-var temp = $(".temp");
-var humidity = $(".humid");
-var wind = $(".windSpeed");
-var uvIndex = $(".uvIndex");
+var temp = $(`.temp`);
+var humidity = $(`.humid`);
+var wind = $(`.windSpeed`);
+var uvIndex = $(`.uvIndex`);
 
 searchButton.on("click", function(e) {
     e.preventdefault();
@@ -39,7 +37,49 @@ if (JSON.parse(localStorage.getItem("history")) === null) {
     console.log("search added to history");
     renderSearchHistory();
 }
-// Append to search history
+
+//Get Weather info
+function findWeather(selectCity) {
+    //Getting API
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${selectCity}&appid=${weatherApiKey}&units=imperial`;
+    // AjAX - send request
+    $.ajax({
+        url: queryURL;
+        method: "GET"
+    })
+    .then(function(weatherInfo) {
+        let cityObject = {
+            cityName = weatherInfo.name,
+            cityTemp = weatherInfo.main.temp,
+            cityHumidity = weatherInfo.main.humidity,
+            cityWindSpeed = weatherInfo.wind.speed,
+            cityUVIndex = weatherInfo.coord,
+        }
+    let queryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${cityObject.cityUVIndex.lat}&lon=${cityObject.cityUVIndex.lon}&APPID=${weatherApiKey}&units=imperial`
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    //Getting UV
+    .then(function(uvData) {
+        if (JSON.parse(localStorage.getItem('history')) ==null) {
+            var searchHistArray = [];
+            if (searchHistArray.indexOf(cityObject.cityName) === 1) {
+            searchHistArray.push(cityObject.cityName);
+            // Saving to local from the search
+            localStorage.setItem("history", JSON.stringify(searchHistArray));
+            var renderSearchHistory(cityObject.cityName);    
+            }
+            else {
+               console.log("Not adding to list. Already appears")
+               renderWeatherData(cityObject.cityName, cityObject.cityTemp, cityObject.cityHumidity, cityObject.cityWindSpeed, cityObject.cityUVIndex);
+               renderSearchHistory(cityObject.cityName);
+            }
+        }
+    })
+
+})
+} 
 
 
 
